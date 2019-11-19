0a872d7b10b7d26f2103ccdd6356c31ce3e34e97
David Hildenbrand <david@redhat.com>
2019-11-19 12:09:33 +0100

    KVM: ZONE_DEVICE pages are no longer marked PG_reserved
    Let's drop the comment and the check now that ZONE_DEVICE pages are no
    longer PG_reserved.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: "Radim Krčmář" <rkrcmar@redhat.com>
    Cc: Sean Christopherson <sean.j.christopherson@intel.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: David Hildenbrand <david@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -179,14 +179,8 @@ bool kvm_is_zone_device_pfn(kvm_pfn_t pfn)
 
 bool kvm_is_reserved_pfn(kvm_pfn_t pfn)
 {
-	/*
-	 * ZONE_DEVICE pages currently set PG_reserved, but from a refcounting
-	 * perspective they are "normal" pages, albeit with slightly different
-	 * usage rules.
-	 */
 	if (pfn_valid(pfn))
-		return PageReserved(pfn_to_page(pfn)) &&
-		       !kvm_is_zone_device_pfn(pfn);
+		return PageReserved(pfn_to_page(pfn));
 
 	return true;
 }
