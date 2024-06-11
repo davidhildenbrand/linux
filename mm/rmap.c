d29b25129c6243bb1fb366878ed777ff4502470c
David Hildenbrand <david@redhat.com>
2024-06-11 15:02:57 +0200

    mm/rmap: cleanup partially-mapped handling in __folio_remove_rmap()
    Let's simplify and reduce code indentation. In the RMAP_LEVEL_PTE case, we
    already check for nr when computing partially_mapped.
    
    For RMAP_LEVEL_PMD, it's a bit more confusing. Likely, we don't need the
    "nr" check, but we could have "nr < nr_pmdmapped" also if we stumbled
    into the "/* Raced ahead of another remove and an add? */" case. So
    let's simply move the nr check in there.
    
    Note that partially_mapped is always false for small folios.
    
    No functional change intended.
    
    Cc: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: David Hildenbrand <david@redhat.com>

--- mm/rmap.c
+++ mm/rmap.c
@@ -1549,11 +1549,12 @@ static __always_inline void __folio_remove_rmap(struct folio *folio,
 			}
 		}
 
-		partially_mapped = nr < nr_pmdmapped;
+		partially_mapped = nr && nr < nr_pmdmapped;
 		break;
 	}
 
-	if (nr) {
+	if (partially_mapped && folio_test_anon(folio) &&
+	    list_empty(&folio->_deferred_list))
 		/*
 		 * Queue anon large folio for deferred split if at least one
 		 * page of the folio is unmapped and at least one page
@@ -1561,10 +1562,7 @@ static __always_inline void __folio_remove_rmap(struct folio *folio,
 		 *
 		 * Check partially_mapped first to ensure it is a large folio.
 		 */
-		if (folio_test_anon(folio) && partially_mapped &&
-		    list_empty(&folio->_deferred_list))
-			deferred_split_folio(folio);
-	}
+		deferred_split_folio(folio);
 	__folio_mod_stat(folio, -nr, -nr_pmdmapped);
 
 	/*
